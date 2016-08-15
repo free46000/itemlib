b51d3eeca02cf576c96356d729cfc7fd1ca8cda0
free46000 <free46000@163.com>
2016-08-16 00:07:11 +0800

    Signed-off-by: free46000 <free46000@163.com>

--- src/main/java/free/com/itemlib/MainActivity.java
+++ src/main/java/free/com/itemlib/MainActivity.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import free.com.itemlib.item.BaseItemAdapter;
 import free.com.itemlib.item.decoration.HorizontalItemDecoration;
+import free.com.itemlib.item.listener.OnItemClickListener;
 import free.com.itemlib.item.listener.OnLoadMoreListener;
 import free.com.itemlib.item.view.ItemViewHolder;
 import free.com.itemlib.item.view.content.Item;
@@ -30,7 +34,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, InputActivity.class));
+        startActivity(new Intent(this, RecyclerActivity.class));
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         baseItemAdapter = new BaseItemAdapter(this);
@@ -89,13 +93,65 @@ public class MainActivity extends Activity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(baseItemAdapter);
         recyclerView.addItemDecoration(new HorizontalItemDecoration(this, R.drawable.list_divider));
+
+        //ItemTouchHelper 用于实现 RecyclerView Item 拖曳效果的类
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                //actionState : action状态类型，有三类 ACTION_STATE_DRAG （拖曳），ACTION_STATE_SWIPE（滑动），ACTION_STATE_IDLE（静止）
+//                int dragFlags = makeFlag(ItemTouchHelper.ACTION_STATE_DRAG, ItemTouchHelper.UP | ItemTouchHelper.DOWN
+//                        | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);//支持上下左右的拖曳
+//                int swipeFlags = makeMovementFlags(ItemTouchHelper.ACTION_STATE_SWIPE, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);//表示支持左右的滑动
+                int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN
+                        | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;//支持上下左右的拖曳
+                int swipeFlags =  ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;//表示支持左右的滑动
+                return makeMovementFlags(dragFlags, swipeFlags);//直接返回0表示不支持拖曳和滑动
+            }
+
+            /**
+             * @param recyclerView attach的RecyclerView
+             * @param viewHolder 拖动的Item
+             * @param target 放置Item的目标位置
+             * @return
+             */
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                int fromPosition = viewHolder.getAdapterPosition();//要拖曳的位置
+                int toPosition = target.getAdapterPosition();//要放置的目标位置
+//                Collections.swap(mData, fromPosition, toPosition);//做数据的交换
+                baseItemAdapter.notifyItemMoved(fromPosition, toPosition);
+                return true;
+            }
+
+            /**
+             * @param viewHolder 滑动移除的Item
+             * @param direction
+             */
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();//获取要滑动删除的Item位置
+//                mData.remove(position);//删除数据
+                baseItemAdapter.notifyItemRemoved(position);
+            }
+
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        baseItemAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(Item item, int location) {
+                Toast.makeText(MainActivity.this,item.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private List<Item> getItemList() {
         List<Item> list = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             if (i % 5 == 0) {
-                list.add(new ItemText(i + "fdsagsaadfdasfasfdasfdasfdasfdgagsaggasg" + i) {
+                list.add(new ItemText(i + "fdsagsajhjhgkghjkjhgkjhghjghadfdasfasfdasfdasfdasfdgagsaggasg" + i) {
                     @Override
                     public boolean isFullSpan() {
                         return true;
@@ -109,7 +165,7 @@ public class MainActivity extends Activity {
 
     static int i;
 
-    class ItemText extends ItemImpl {
+    public static class ItemText extends ItemImpl {
         private String value;
 
         public ItemText(String value) {
